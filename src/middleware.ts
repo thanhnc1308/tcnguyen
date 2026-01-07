16e10182a7a8d1b2ed3145d87d6e450377191b9f
Thanh Nguyen <thanh.nguyencong@go1.com>
2026-01-07 22:45:56 +0700

    Fix trusted host and temp fix mongoose in Edge runtime issue

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,3 +4,8 @@ export const config = {
   // https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
   matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
 };
+
+// Force middleware to use Node.js runtime instead of Edge
+// TODO: move auth db checking to Edge compatible solution
+// and remove this line
+export const runtime = 'nodejs';
