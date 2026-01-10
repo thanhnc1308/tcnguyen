dc303f49f1079a186ca00a43376d4c359f21c577
Thanh Nguyen <thanh.nguyencong@go1.com>
2026-01-10 15:15:47 +0700

    Add debug log

--- src/server/db/mongodb.ts
+++ src/server/db/mongodb.ts
@@ -19,7 +19,11 @@ async function dbConnect() {
     return globalWithMongoose.mongooseConnection;
   }
 
-  console.log('Connecting to MongoDB...');
+  console.log('Connecting to MongoDB...', {
+    uri: process.env.MONGODB_URI,
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+    ADMIN_EMAILS: process.env.ADMIN_EMAILS,
+  });
 
   const opts = {
     bufferCommands: true, // Enable buffering so queries wait for connection
